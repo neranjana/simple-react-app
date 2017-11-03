b77d0f13d9c6155a89c0510bf5ca450ca01ed2f6
Neranjana Karunaratne <neranjana@gmail.com>
2017-11-03 15:01:48 +1100

    Simple event handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import Parent from './components/parent';
-import Child from './components/child';
 
 class App extends Component {
   render() {
--- src/components/child.js
+++ src/components/child.js
@@ -4,9 +4,18 @@ class Child extends Component {
     render() {
         return (
             <div className='child'>
-                This is Child
+                This is Child <br />
+                <input onChange={this.onInputChange} />
+                <br />
+                {/* another way to do the same */}
+                <input onChange={event => console.log(event.target.value)} />
             </div>    
         );
+    }
+
+    // the parameter event can be named as anything. e.g. eventObject, etc.
+    onInputChange(event) {
+        console.log(event.target.value);
     }
 }
 
